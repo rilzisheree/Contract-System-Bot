e4b351250d07963ffbc0f7f6e7b423f2a9a6bb5a
rilzisheree <160282181+rilzisheree@users.noreply.github.com>
2026-05-06 19:49:18 +0300

    Add files via upload

---
+++ src/commands/sendtext.js
@@ -0,0 +1,38 @@
+import {
+  SlashCommandBuilder,
+  ModalBuilder,
+  ActionRowBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('sendtext')
+  .setDescription('Send a text message to another player.');
+
+export async function execute(interaction) {
+  const modal = new ModalBuilder()
+    .setCustomId('sendtext_modal')
+    .setTitle('Send a Text Message');
+
+  const recipientInput = new TextInputBuilder()
+    .setCustomId('recipient_id')
+    .setLabel('Which contact do you send a text to?')
+    .setPlaceholder('Send their Discord ID')
+    .setStyle(TextInputStyle.Short)
+    .setRequired(true);
+
+  const messageInput = new TextInputBuilder()
+    .setCustomId('message_content')
+    .setLabel('What does your text say?')
+    .setPlaceholder('Text | Lore Name | Discord Username')
+    .setStyle(TextInputStyle.Paragraph)
+    .setRequired(true);
+
+  modal.addComponents(
+    new ActionRowBuilder().addComponents(recipientInput),
+    new ActionRowBuilder().addComponents(messageInput)
+  );
+
+  await interaction.showModal(modal);
+}
